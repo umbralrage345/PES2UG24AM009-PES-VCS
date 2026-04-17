c01ac2b2c79fcbf2903e6ce3460507a13319a337
Abhyudaya <abhyudayamhegde@gmail.com>
2026-04-18 04:38:48 +0530

    Implemented index_load function

--- index.c
+++ index.c
@@ -135,10 +135,34 @@ int index_status(const Index *index) {
 //
 // Returns 0 on success, -1 on error.
 int index_load(Index *index) {
-    // TODO: Implement index loading
-    // (See Lab Appendix for logical steps)
-    (void)index;
-    return -1;
+    index->count = 0;
+
+    FILE *f = fopen(INDEX_FILE, "r");
+    if (!f)
+        return 0;
+
+    while (index->count < MAX_INDEX_ENTRIES) {
+        IndexEntry *e = &index->entries[index->count];
+
+        char hex[HASH_HEX_SIZE + 1];
+
+        int rc = fscanf(f, "%o %64s %lu %u %511s",
+                        &e->mode,
+                        hex,
+                        &e->mtime_sec,
+                        &e->size,
+                        e->path);
+
+        if (rc != 5)
+            break;
+
+        hex_to_hash(hex, &e->hash);
+        index->count++;
+    }
+
+    fclose(f);
+    return 0;
+}
 }
 
 // Save the index to .pes/index atomically.
